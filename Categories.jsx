597690b21d0de449441fed80611987cc447e6218
Vika <slarkmidd@mail.ru>
2025-11-27 01:13:37 +0300

    обнова

---
+++ Categories.jsx
@@ -0,0 +1,24 @@
+import React, { act } from "react";
+import { Value } from "sass";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые',]
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  }
+
+  return (
+    <div className="categories">
+      <ul>
+        {
+          categories.map((value, i) => (<li onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ""}>{value}</li>))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default Categories
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // чтобы логику реакта переобразовать в html
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( <App />);
