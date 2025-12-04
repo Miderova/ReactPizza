5c914534711c3689a9779e8d7080d5680821055f
Vika <slarkmidd@mail.ru>
2025-12-04 07:10:41 +0300

    11-12

--- src/App.js
+++ src/App.js
@@ -6,21 +6,47 @@ import Home from "./pages/Home"
 import Cart from "./pages/Cart"
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/filterSlice'
+
+export const SearchContext = React.createContext();
 
 
 function App() {
-  const[searchValue, setSearchValue] = React.useState('');
+  const { searchValue, setSearchValue } = React.useState('');
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
+
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
+
+
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
           <Routes>
-          <Route path="/" element={<Home searchValue={searchValue}/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="*" element={<NotFound/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
-      </div>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Search.module.scss";
+import { SearchContext } from "../../App";
 
-const Search = ({ searchValue, setSearchValue }) => {
+
+const Search = () => {
+
+  const {searchValue,setSearchValue} = React.useContext(SearchContext);
+
   return (
     <div className={styles.root}>
       <svg
